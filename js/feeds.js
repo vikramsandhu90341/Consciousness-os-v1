6ad67a99688200dec6bf0ef3e7da6413a0ed962b
vikramsandhu90341 <vikramsandhu90341@gmail.com>
2026-03-13 12:47:51 +0530

    Update feeds.js

--- js/feeds.js
+++ js/feeds.js
@@ -1,1 +1,41 @@
+async function loadWorld(){
 
+const rss="https://nitter.net/ReutersWorld/rss";
+
+const url="https://api.rss2json.com/v1/api.json?rss_url="+rss;
+
+const res=await fetch(url);
+
+const data=await res.json();
+
+render(data);
+
+}
+
+async function loadScience(){
+
+const rss="https://nitter.net/nature/rss";
+
+const url="https://api.rss2json.com/v1/api.json?rss_url="+rss;
+
+const res=await fetch(url);
+
+const data=await res.json();
+
+render(data);
+
+}
+
+function render(data){
+
+let html="";
+
+data.items.slice(0,10).forEach(a=>{
+
+html+=`<p><a href="${a.link}" target="_blank">${a.title}</a></p>`;
+
+});
+
+document.getElementById("feed").innerHTML=html;
+
+}
